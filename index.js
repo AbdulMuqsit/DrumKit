ac3bba33392f2c9a992fbdd8869aec5fc03d8bcd
Abdul Muqsit Zargar <96869359+AbdulMuqsit@users.noreply.github.com>
2023-02-16 20:15:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+// document.querySelector("button").addEventListener("click",handleclick);
+// function handleclick()
+// {
+//     alert("I got clicked!");
+// }
+var numberofdrums = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberofdrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+   
+        // alert("I got clicked!");
+        // var audio=new Audio("tom-4.mp3");
+        // audio.play();
+        // this.style.color="white";
+        var buttoninnerHTML = this.innerHTML;
+        switch (buttoninnerHTML) {
+            case "w":
+
+                var tom1 = new Audio("tom-1.mp3");
+                tom1.play();
+                break;
+                case "a":
+                    var tom2=new Audio("tom-2.mp3");
+                    tom2.play();
+                    break;
+                    case "s":
+                        var tom3=new Audio("tom-3.mp3");
+                        tom3.play();
+                        break;
+                        case "d":
+                            var tom4=new Audio("tom-4.mp3");
+                            tom4.play();
+                            break;
+                        case "j":
+                            var snare=new Audio("snare.mp3");
+                            snare.play();
+                            break;
+                            case "k":
+                                var crash=new Audio("crash.mp3");
+                                crash.play();
+                                break;
+                                case "l":
+                                    var kick=new Audio("kick-bass.mp3");
+                                    kick.play();
+                                    break;
+                                    default:console.log(buttoninnerHTML);
+
+        }
+    });
+//     document.addEventListener("keypress",function()
+// {
+//     alert("Key was pressed!");
+// });
+  
+}
